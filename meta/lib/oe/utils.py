d5fc487151777d61836766beb889481f0e1e9e49
Peter Kjellerstedt <peter.kjellerstedt@axis.com>
2014-08-28 17:11:03 +0200

    lib/oe/utils: Make multiprocess_exec() return anything
    The variable "results" was accidentally used for multiple different
    things at the same time, which unintentionally discarded anything that
    was supposed to be returned from the function...
    
    Signed-off-by: Peter Kjellerstedt <peter.kjellerstedt@axis.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- meta/lib/oe/utils.py
+++ meta/lib/oe/utils.py
@@ -180,11 +180,11 @@ def multiprocess_exec(commands, function):
     imap = pool.imap(function, commands)
 
     try:
-        results = list(imap)
+        res = list(imap)
         pool.close()
         pool.join()
         results = []
-        for result in results:
+        for result in res:
             if result is not None:
                 results.append(result)
         return results
