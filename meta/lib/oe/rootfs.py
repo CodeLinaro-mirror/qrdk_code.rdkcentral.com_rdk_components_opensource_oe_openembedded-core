183322af156301b3cf539b5c9c94e2165fa3a3e1
Herve Jourdain <herve.jourdain@beechwoods.com>
2017-08-06 16:55:30 +0800

    RDKCMF-7553 When adding "nfs-utils-client" to the image, do_rootfs fails [Krogoth and Morty]
    Change-Id: I99e86cc53cd16d74434d1326ab5860d023979871

--- meta/lib/oe/rootfs.py
+++ meta/lib/oe/rootfs.py
@@ -909,7 +909,7 @@ class OpkgRootfs(DpkgOpkgRootfs):
 
         self.pm.install_complementary()
 
-        self._setup_dbg_rootfs(['/etc', '/var/lib/opkg', '/usr/lib/ssl'])
+        self._setup_dbg_rootfs(['/etc', '/var/lib/opkg', '/usr/lib/ssl', '/var/lib/nfs'])
 
         execute_pre_post_process(self.d, opkg_post_process_cmds)
 
